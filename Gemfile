998a93e5114e64f2789f0930d11d6067d4192bfc
SuebSaifi <sueb18-cs@danskar.org>
2022-07-01 19:03:52 +0530

    Implement Scraper Class

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
-gem "nokogiri"
-gem "httparty"
+gem 'nokogiri', '~> 1.13', '>= 1.13.6'
+# gem "httparty"
 gem "byebug"
 gem 'watir', '~> 7.1'
 gem 'webdrivers', '~> 5.0'
+gem 'open-uri'
+gem 'httparty', '~> 0.20.0' 
--- scraper.rb
+++ scraper.rb
@@ -4,29 +4,32 @@ require 'byebug'
 require 'watir'
 require 'webdrivers'
 require 'csv'
-
-def scraper  
-page=1
-    browser = Watir::Browser.new
-    (10).times do 
-    browser.goto "https://www.glassdoor.co.in/Reviews/new-delhi-reviews-SRCH_IL.0,9_IM1083_IP#{page}.htm"
-    page+=1
-    
-    # url = 
-    
-    # unparsed_page = HTTParty.get(url)
-    parsed_page=Nokogiri::HTML(browser.html)  
-    # debugger
-    company_review= parsed_page.css('div.single-company-result.module div.row div.col-lg-5 div.row div.ei-contribution-wrap a.reviews span.num').text
-    company_name= parsed_page.css('div.single-company-result.module div.row div.col-lg-7 div.col-9 h2  a').text
-    
-    # File.open("File.csv", "a+") {|f| f.write "#{company_review}" }
-    CSV.open("File.csv", "a+") do |csv|    
-        csv << [:company, company_name]
-        csv << [:review, company_review]
-        
+class Scraper 
+    def initialize
+        @parsed_data
+        @total_count
     end
-end
-    browser .close
-end
-scraper
+     def scrap
+        browser = Watir::Browser.new
+
+        browser.goto "https://www.glassdoor.co.in/Reviews/new-delhi-reviews-SRCH_IL.0,9_IM1083_IP#{page_first}.htm"
+        @parsed_data = Nokogiri::HTML(browser.html)
+        per_page = @parsed_data.css('div.single-company-result.module').count
+        total=@parsed_data.css('div.pb-lg-xxl strong').last.text.split()[0].gsub(',',"").to_i
+        @total_count=(total.to_f/per_page.to_f).to_i
+        browser.close
+     end
+     def csv    
+        fatched_data = @parsed_data.css('div.single-company-result.module div.row div.col-lg-7 div.col-9 h2' )
+        
+        CSV.open("File1.csv", "a+",headers:["Company","Review"]) do |csv|
+            fatched_data.each do |cv|
+                csv << [cv.css("a").text.split("\n").join,cv.css('div.ratingsSummary span.bigRating').text.split("\n").join.to_f]      
+            end
+        end
+    end 
+end 
+
+scraper = Scraper.new
+scraper.scrap
+scraper.csv
